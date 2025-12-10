398eef42ad3afa64f4bd8177ae72cc826f1a28cf
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-12-10 01:33:42 +0000

    sum stuff

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("why doesnt this work?")
+  .then(response => response.text())
+  .then(text => {
+    console.log("Fetched string:", text);
+  })
+fetch("./api/[0][12][22][45]")
+  .then(response => response.text())
+  .then(text => {
+    console.log("Fetched string:", text);
+  })
