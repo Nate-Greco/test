1cbf2fb7e3365af71ea947d234585d0c59f1583c
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-12-10 03:53:07 +0000

    ?

--- pa/script.js
+++ pa/script.js
@@ -1,6 +1,6 @@
 document.getElementsByTagName("button")[0].addEventListener("click", function() {
     var ans = prompt("What is your answer?");
-    var result = await fetch("https://white-paper-d4da.yyznate.workers.dev/?pw=${encodeURIComponent(ans)}");
+    var result = fetch("https://white-paper-d4da.yyznate.workers.dev/?pw=${encodeURIComponent(ans)}").then(response => response.text());
     if (result == "y") {
         alert("Correct! Redirecting you to your prize page.");
     }
