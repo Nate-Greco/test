47d7fa1eddbd02986743ddd6f2692661de5cfcda
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-12-10 01:50:24 +0000

    page1

---
+++ page1/script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+    document.getElementsByTagName("h2")[0].style.color = "red";
+  });
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch("why doesnt this work?")
   .then(text => {
     console.log("Fetched string:", text);
   })
-fetch("./api/[0][12][22][45]")
+fetch("./api/[0][12][22][45].txt")
   .then(response => response.text())
   .then(text => {
     console.log("Fetched string:", text);
