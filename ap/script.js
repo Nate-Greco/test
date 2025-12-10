03a35ec4018cf3fc681de46937bc05af6989efa3
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-12-10 04:29:07 +0000

    audio stuff

---
+++ ap/script.js
@@ -0,0 +1,13 @@
+document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    var ans = prompt("What is your answer?");
+
+    var result = fetch(`https://white-paper-d4da.yyznate.workers.dev/?pw=${encodeURIComponent(ans)}`)
+        .then(response => response.text())
+        .then(res => {
+            console.log(res);
+            if (res == "y") {
+                alert("Correct! Redirecting you to your prize page.");
+                window.open("../ap");
+            }
+        })
+  });
