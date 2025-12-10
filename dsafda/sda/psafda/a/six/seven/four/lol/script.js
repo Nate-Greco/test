cedf7dde21209f44b322577a033d98284083c803
Nate Greco <34966406+Nate-Greco@users.noreply.github.com>
2025-12-10 02:09:58 +0000

    final page

---
+++ dsafda/sda/psafda/a/six/seven/four/lol/script.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    alert("Tell this secret code to Jonathan and he will reward you... if you're first! The code is: 8867678867");
+  });
